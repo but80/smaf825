f748bebe94ec83fd622e1b9eefdbd5236ceaa456
but80 <mersenne.sister@gmail.com>
2017-10-06 21:32:49 +0900

    playコマンドを簡略化

--- subcmd/play.go
+++ subcmd/play.go
@@ -11,81 +11,56 @@ import (
 var Play = cli.Command{
 	Name:      "play",
 	Aliases:   []string{"p"},
-	Usage:     "Plays SMAF format files",
-	ArgsUsage: " ",
-	Subcommands: []cli.Command{
-		{
-			Name:      "mmf",
-			Aliases:   []string{"m"},
-			Usage:     "Plays MMF files",
-			ArgsUsage: "<device> <filename>",
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "state, s",
-					Usage: `Show state`,
-				},
-				cli.IntFlag{
-					Name:  "volume, v",
-					Usage: `Master volume (0..63)`,
-					Value: 48,
-				},
-				cli.IntFlag{
-					Name:  "gain, g",
-					Usage: `Analog gain (0..3)`,
-					Value: 1,
-				},
-				cli.IntFlag{
-					Name:  "seqvol, V",
-					Usage: `SeqVol (0..31)`,
-					Value: 16,
-				},
-				cli.IntFlag{
-					Name:  "loop, l",
-					Usage: `Loop count (0: infinite)`,
-					Value: 1,
-				},
-			},
-			Action: func(ctx *cli.Context) error {
-				if ctx.NArg() < 2 || ctx.Int("loop") < 0 ||
-					ctx.Int("volume") < 0 || 63 < ctx.Int("volume") ||
-					ctx.Int("gain") < 0 || 3 < ctx.Int("gain") ||
-					ctx.Int("seqvol") < 0 || 31 < ctx.Int("seqvol") {
-					cli.ShowCommandHelp(ctx, "mmf")
-					os.Exit(1)
-				}
-				args := ctx.Args()
-				mmf, err := chunk.NewFileChunk(args[1])
-				if err != nil {
-					return cli.NewExitError(err, 1)
-				}
-				//fmt.Println(mmf.String())
-				q := sequencer.Sequencer{
-					DeviceName: args[0],
-					ShowState:  ctx.Bool("state"),
-				}
-				err = q.Play(mmf, ctx.Int("loop"), ctx.Int("volume"), ctx.Int("gain"), ctx.Int("seqvol"))
-				if err != nil {
-					return cli.NewExitError(err, 1)
-				}
-				return nil
-			},
+	Usage:     "Plays SMAF format files (.mmf|.spf)",
+	ArgsUsage: "<device> <filename>",
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "state, s",
+			Usage: `Show state`,
 		},
-		{
-			Name:      "test",
-			Aliases:   []string{"t"},
-			Usage:     "Test",
-			ArgsUsage: "<device>",
-			Action: func(ctx *cli.Context) error {
-				if ctx.NArg() < 1 {
-					cli.ShowCommandHelp(ctx, "test")
-					os.Exit(1)
-				}
-				err := sequencer.Test(ctx.Args()[0])
-				if err != nil {
-					return cli.NewExitError(err, 1)
-				}
-				return nil
-			},
+		cli.IntFlag{
+			Name:  "volume, v",
+			Usage: `Master volume (0..63)`,
+			Value: 48,
 		},
+		cli.IntFlag{
+			Name:  "gain, g",
+			Usage: `Analog gain (0..3)`,
+			Value: 1,
+		},
+		cli.IntFlag{
+			Name:  "seqvol, V",
+			Usage: `SeqVol (0..31)`,
+			Value: 16,
+		},
+		cli.IntFlag{
+			Name:  "loop, l",
+			Usage: `Loop count (0: infinite)`,
+			Value: 1,
+		},
+	},
+	Action: func(ctx *cli.Context) error {
+		if ctx.NArg() < 2 || ctx.Int("loop") < 0 ||
+			ctx.Int("volume") < 0 || 63 < ctx.Int("volume") ||
+			ctx.Int("gain") < 0 || 3 < ctx.Int("gain") ||
+			ctx.Int("seqvol") < 0 || 31 < ctx.Int("seqvol") {
+			cli.ShowCommandHelp(ctx, "play")
+			os.Exit(1)
+		}
+		args := ctx.Args()
+		mmf, err := chunk.NewFileChunk(args[1])
+		if err != nil {
+			return cli.NewExitError(err, 1)
+		}
+		//fmt.Println(mmf.String())
+		q := sequencer.Sequencer{
+			DeviceName: args[0],
+			ShowState:  ctx.Bool("state"),
+		}
+		err = q.Play(mmf, ctx.Int("loop"), ctx.Int("volume"), ctx.Int("gain"), ctx.Int("seqvol"))
+		if err != nil {
+			return cli.NewExitError(err, 1)
+		}
+		return nil
 	},
 }
